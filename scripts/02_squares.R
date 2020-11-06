e12958f495903a78426483a5eeb8815d9c9d349a
jvcasillas <joseph.casillas@rutgers.edu>
2020-11-06 14:53:10 -0500

    make low complexity squares a bit wider and change movement directions

--- scripts/02_squares.R
+++ scripts/02_squares.R
@@ -28,20 +28,62 @@ source(here::here("scripts", "01_helpers.R"))
 # Create 2x2 low complexity dataframe
 low_complexity_square_df <- tribble(
   ~'x',  ~'y', ~'color', ~'phase',
-  0.25,  0.75,  "black",        1,
-  0.75,  0.75,  "black",        2,
-  0.75,  0.25,  "black",        3,
-  0.75,  0.25,  "black",        1,
-  0.25,  0.25,  "black",        2,
-  0.25,  0.75,  "black",        3)
+  0.25, -0.75,  "black",        1,
+  0.25, -0.25,  "black",        2,
+  0.25,  0.25,  "black",        3,
+  0.25, -0.25,  "white",        1,
+  0.25,  0.25,  "white",        2,
+  0.25,  0.75,  "white",        3,
+  0.25,  0.25,  "black",        1,
+  0.25,  0.75,  "black",        2,
+  0.25,  1.25,  "black",        3,
+  0.25,  0.75,  "white",        1,
+  0.25,  1.25,  "white",        2,
+  0.25,  1.75,  "white",        3,
+  0.75, -0.75,  "white",        1,
+  0.75, -0.25,  "white",        2,
+  0.75,  0.25,  "white",        3,
+  0.75, -0.25,  "black",        1,
+  0.75,  0.25,  "black",        2,
+  0.75,  0.75,  "black",        3,
+  0.75,  0.25,  "white",        1,
+  0.75,  0.75,  "white",        2,
+  0.75,  1.25,  "white",        3,
+  0.75,  0.75,  "black",        1,
+  0.75,  1.25,  "black",        2,
+  0.75,  1.75,  "black",        3,
+ -0.32, -0.75,  "white",        1,
+ -0.32, -0.25,  "white",        2,
+ -0.32,  0.25,  "white",        3,
+ -0.32, -0.25,  "black",        1,
+ -0.32,  0.25,  "black",        2,
+ -0.32,  0.75,  "black",        3,
+ -0.32,  0.25,  "white",        1,
+ -0.32,  0.75,  "white",        2,
+ -0.32,  1.25,  "white",        3,
+ -0.32,  0.75,  "black",        1,
+ -0.32,  1.25,  "black",        2,
+ -0.32,  1.75,  "black",        3,
+  1.25, -0.75,  "black",        1,
+  1.25, -0.25,  "black",        2,
+  1.25,  0.25,  "black",        3,
+  1.25, -0.25,  "white",        1,
+  1.25,  0.25,  "white",        2,
+  1.25,  0.75,  "white",        3,
+  1.25,  0.25,  "black",        1,
+  1.25,  0.75,  "black",        2,
+  1.25,  1.25,  "black",        3,
+  1.25,  0.75,  "white",        1,
+  1.25,  1.25,  "white",        2,
+  1.25,  1.75,  "white",        3)
 
 # Simple static plot
 low_complexity_static_square <- low_complexity_square_df %>% 
-  filter(phase == 2) %>% 
   ggplot(., aes(x = x, y = y, color = color)) + 
-    geom_point(pch = 15, size = 85, show.legend = F, color = "black") + 
-    coord_cartesian(xlim = c(0, 1), ylim = c(0, 1), expand = F) + 
-    theme_clear(bg_color = "white") 
+    geom_point(pch = 15, size = 85, show.legend = F) + 
+    scale_color_manual(values = c("black", "white")) + 
+    coord_cartesian(xlim = c(-0.11, 1.05), ylim = c(0, 1), expand = F) + 
+    theme_clear(bg_color = "grey") 
 
 # Save as .png
 ggsave("low_complexity_static_square.png", low_complexity_static_square, 
@@ -50,9 +92,10 @@ ggsave("low_complexity_static_square.png", low_complexity_static_square,
 # Add circular movement
 low_complexity_movement_square <- low_complexity_square_df %>% 
   ggplot(., aes(x = x, y = y, color = color)) + 
-    geom_point(pch = 15, size = 85, show.legend = F, color = "black") + 
-    coord_cartesian(xlim = c(0, 1), ylim = c(0, 1), expand = F) + 
-    theme_clear(bg_color = "white") + 
+    geom_point(pch = 15, size = 85, show.legend = F) + 
+    scale_color_manual(values = c("black", "white")) + 
+    coord_cartesian(xlim = c(-0.11, 1.05), ylim = c(0, 1), expand = F) + 
+    theme_clear(bg_color = "transparent") + 
     transition_states(phase, transition_length = 1, state_length = 0, wrap = F)
 
 # Animate
