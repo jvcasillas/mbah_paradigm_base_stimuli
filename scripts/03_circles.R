cfcc9ed4715b9bb23c4b24a420aeee148cadd168
jvcasillas <joseph.casillas@rutgers.edu>
2020-12-02 18:35:31 -0500

    update low complexity circles

--- scripts/03_circles.R
+++ scripts/03_circles.R
@@ -23,48 +23,176 @@ source(here::here("scripts", "01_helpers.R"))
 
 
 
-# Low complexity circle ------------------------------------------------------
+# Low complexity static circles -----------------------------------------------
 
-# Static
-low_complexity_static_circle <- make_circle(diameter = 0.75, npoints = 200) %>% 
-  mutate(position = "outer") %>% 
-  add_row(x = 0, y = 0, position = "inner") %>% 
-  ggplot(., aes(x = x, y = y, size = position)) + 
-    geom_point(show.legend = F) + 
-    scale_size_manual(values = c(50, 10)) + 
+circle_low_complexity_static <- function(color = 1) {
+
+  static_circle_data <- tibble(
+    x = c(0, 0, 0), 
+    y = c(0, 0, 0), 
+    size = c(120, 20, 120), 
+    color = c(c_picker(color)[1], c_picker(color)[2], c_picker(color)[2]),
+    shape = c(16, 21, 21))
+  
+  p_static_circle <- static_circle_data %>% 
+  ggplot(., aes(x = x, y = y)) + 
+    geom_point(aes(size = size, color = color, shape = shape), 
+      show.legend = F, stroke = 30) +
+    scale_size_identity() + 
+    scale_color_identity() + 
+    scale_shape_identity() + 
     coord_cartesian(xlim = c(-0.4, 0.4), ylim = c(-0.4, 0.4), expand = F) + 
     theme_clear()
 
+  return(p_static_circle)
+}
+
+circle_low_complexity_static_c1 <- circle_low_complexity_static(color = 1)
+circle_low_complexity_static_c2 <- circle_low_complexity_static(color = 2)
+circle_low_complexity_static_c3 <- circle_low_complexity_static(color = 3)
+circle_low_complexity_static_c4 <- circle_low_complexity_static(color = 4)
+
 # Save as .png
-ggsave("low_complexity_static_circle.png", low_complexity_static_circle, 
+ggsave("circle_low_complexity_static_c1.png", circle_low_complexity_static_c1, 
+  path = here("stim"), units = "in", height = 5, width = 5)
+ggsave("circle_low_complexity_static_c2.png", circle_low_complexity_static_c2, 
+  path = here("stim"), units = "in", height = 5, width = 5)
+ggsave("circle_low_complexity_static_c3.png", circle_low_complexity_static_c3, 
+  path = here("stim"), units = "in", height = 5, width = 5)
+ggsave("circle_low_complexity_static_c4.png", circle_low_complexity_static_c4, 
   path = here("stim"), units = "in", height = 5, width = 5)
 
-# Expanding movement
-low_complexity_movement_circle <- bind_rows(
-    make_circle(diameter = 0.15, npoints = 200) %>% mutate(s = 1), 
-    make_circle(diameter = 0.75, npoints = 200) %>% mutate(s = 2)) %>% 
-  mutate(position = "outer")  %>% 
-  add_row(x = 0, y = 0, s = 1, position = "inner") %>% 
-  add_row(x = 0, y = 0, s = 2, position = "inner") %>% 
-  ggplot(., aes(x = x, y = y, size = position)) + 
-    geom_point(show.legend = F) + 
-    scale_size_manual(values = c(50, 10)) + 
+# -----------------------------------------------------------------------------
+
+
+
+
+# Low complexity mobile circles -----------------------------------------------
+
+circle_low_complexity_movement <- function(color = 1) {
+
+  movement_circle_data <- tribble(
+  ~"x", ~"y", ~"size", ~"color", ~"shape", ~"stroke", ~"s", 
+    0, 0,   0, c_picker(color)[1], 21,  0, 1, 
+    0, 0,   0, c_picker(color)[2], 21,  0, 1, 
+    0, 0,   0, c_picker(color)[1], 21, 30, 1, 
+    0, 0,  20, c_picker(color)[2], 21, 30, 1, 
+    0, 0,  60, c_picker(color)[1], 21, 45, 1, 
+    0, 0, 120, c_picker(color)[2], 21, 30, 1, 
+    0, 0,   0, c_picker(color)[1], 21,  0, 2, 
+    0, 0,   0, c_picker(color)[2], 21, 30, 2, 
+    0, 0,  20, c_picker(color)[1], 21, 30, 2, 
+    0, 0,  60, c_picker(color)[2], 21, 45, 2, 
+    0, 0, 120, c_picker(color)[1], 21, 30, 2, 
+    0, 0, 160, c_picker(color)[2], 21,  0, 2, 
+    0, 0,   0, c_picker(color)[1], 21, 30, 3, 
+    0, 0,  20, c_picker(color)[2], 21, 30, 3, 
+    0, 0,  60, c_picker(color)[1], 21, 45, 3, 
+    0, 0, 120, c_picker(color)[2], 21, 30, 3, 
+    0, 0, 160, c_picker(color)[1], 21,  0, 3, 
+    0, 0, 160, c_picker(color)[2], 21,  0, 3, 
+  ) 
+
+  p_movement_circle <- movement_circle_data %>% 
+  ggplot() + 
+    geom_point(aes(x = x, y = y, size = size, color = color, shape = shape, 
+      stroke = stroke), 
+      show.legend = F) +
+    scale_size_identity() + 
+    scale_color_identity() + 
+    scale_shape_identity() + 
     coord_cartesian(xlim = c(-0.4, 0.4), ylim = c(-0.4, 0.4), expand = F) + 
-    theme_clear() + 
-    transition_states(s, transition_length = 0.5, state_length = 0, wrap = F) + 
-    enter_fade() +
-    exit_fade()
+    theme_clear() 
+  
+  return(p_movement_circle)
+}
+
+low_complexity_movement_circle_c1 <- circle_low_complexity_movement(color = 1) + 
+    transition_states(s, transition_length = 0.5, state_length = 0, wrap = F) 
+low_complexity_movement_circle_c2 <- circle_low_complexity_movement(color = 2) + 
+    transition_states(s, transition_length = 0.5, state_length = 0, wrap = F) 
+low_complexity_movement_circle_c3 <- circle_low_complexity_movement(color = 3) + 
+    transition_states(s, transition_length = 0.5, state_length = 0, wrap = F) 
+low_complexity_movement_circle_c4 <- circle_low_complexity_movement(color = 4) + 
+    transition_states(s, transition_length = 0.5, state_length = 0, wrap = F) 
 
 # Animate
-low_complexity_movement_circle_p <- animate(low_complexity_movement_circle, 
-                              fps = 100, duration = 1.5, 
-                              device = "png")
+low_complexity_movement_circle_c1_p <- 
+  animate(low_complexity_movement_circle_c1, fps = 100, duration = 1.5, device = "png")
+low_complexity_movement_circle_c2_p <- 
+  animate(low_complexity_movement_circle_c2, fps = 100, duration = 1.5, device = "png")
+low_complexity_movement_circle_c3_p <- 
+  animate(low_complexity_movement_circle_c3, fps = 100, duration = 1.5, device = "png")
+low_complexity_movement_circle_c4_p <- 
+  animate(low_complexity_movement_circle_c4, fps = 100, duration = 1.5, device = "png")
 
 # Save as .gif
-anim_save(here("stim", "low_complexity_movement_circle.gif"), 
-  low_complexity_movement_circle_p)
+anim_save(here("stim", "circle_low_complexity_movement_c1.gif"), 
+  low_complexity_movement_circle_c1_p)
+anim_save(here("stim", "circle_low_complexity_movement_c2.gif"), 
+  low_complexity_movement_circle_c2_p)
+anim_save(here("stim", "circle_low_complexity_movement_c3.gif"), 
+  low_complexity_movement_circle_c3_p)
+anim_save(here("stim", "circle_low_complexity_movement_c4.gif"), 
+  low_complexity_movement_circle_c4_p)
 
 # -----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+# STOPED HERE... TODO... create high complexity images and gifs, resize squares
+
+
+
+low_complexity_movement_circle <- 
+  tribble(
+  ~"x", ~"y", ~"size", ~"color", ~"shape", ~"stroke", ~"s", 
+    0, 0,   0, c_picker(1)[1], 16, 15, 1, 
+    0, 0,  10, c_picker(1)[1], 16, 15, 2, 
+    0, 0,  30, c_picker(1)[1], 16, 15, 3, 
+    0, 0,   0, c_picker(1)[2], 21, 15, 1, 
+    0, 0,  20, c_picker(1)[2], 21, 15, 2, 
+    0, 0,  40, c_picker(1)[2], 21, 15, 3, 
+    0, 0,  20, c_picker(1)[1], 21, 15, 1, 
+    0, 0,  40, c_picker(1)[1], 21, 15, 2, 
+    0, 0,  60, c_picker(1)[1], 21, 15, 3, 
+    0, 0,  40, c_picker(1)[2], 21, 15, 1, 
+    0, 0,  60, c_picker(1)[2], 21, 15, 2, 
+    0, 0,  80, c_picker(1)[2], 21, 15, 3, 
+    0, 0,  60, c_picker(1)[1], 21, 15, 1, 
+    0, 0,  80, c_picker(1)[1], 21, 15, 2, 
+    0, 0, 100, c_picker(1)[1], 21, 15, 3, 
+    0, 0,  80, c_picker(1)[2], 21, 15, 1, 
+    0, 0, 100, c_picker(1)[2], 21, 15, 2, 
+    0, 0, 120, c_picker(1)[2], 21, 15, 3, 
+    0, 0, 100, c_picker(1)[1], 21, 15, 1, 
+    0, 0, 120, c_picker(1)[1], 21, 15, 2, 
+    0, 0, 140, c_picker(1)[1], 21,  0, 3, 
+    0, 0, 120, c_picker(1)[2], 21, 15, 1, 
+    0, 0, 140, c_picker(1)[2], 21,  0, 2, 
+    0, 0, 140, c_picker(1)[2], 21,  0, 3, 
+) %>% 
+  ggplot() + 
+    geom_point(aes(x = x, y = y, size = size, color = color, shape = shape, 
+      stroke = stroke), 
+      show.legend = F) +
+    scale_size_identity() + 
+    scale_color_identity() + 
+    scale_shape_identity() + 
+    coord_cartesian(xlim = c(-0.4, 0.4), ylim = c(-0.4, 0.4), expand = F) + 
+    theme_clear() + 
+    transition_states(s, transition_length = 0.5, state_length = 0, wrap = F) 
+
 
 
 
@@ -72,19 +200,19 @@ anim_save(here("stim", "low_complexity_movement_circle.gif"),
 # High complexity circle -----------------------------------------------------
 
 # Static
-high_complexity_static_circle <- bind_rows(
-  make_circle(diameter = 0.01, npoints = 200) %>% mutate(s = 1), 
-  make_circle(diameter = 0.25, npoints = 200) %>% mutate(s = 1),
-  make_circle(diameter = 0.50, npoints = 200) %>% mutate(s = 1), 
-  make_circle(diameter = 0.25, npoints = 200) %>% mutate(s = 2), 
-  make_circle(diameter = 0.50, npoints = 200) %>% mutate(s = 2), 
-  make_circle(diameter = 0.75, npoints = 200) %>% mutate(s = 2)) %>% 
-  mutate(position = "outer") %>% 
-  add_row(x = 0, y = 0, s = 1, position = "inner") %>% 
-  add_row(x = 0, y = 0, s = 2, position = "inner") %>% 
-  ggplot(., aes(x = x, y = y, size = position)) + 
-    geom_point(show.legend = F) + 
-    scale_size_manual(values = c(10, 3)) + 
+high_complexity_static_circle <- 
+  tibble(
+    x = c(0, 0, 0, 0, 0), 
+    y = c(0, 0, 0, 0, 0), 
+    size = c(140, seq(20, 140, length.out = 4)), 
+    ident = c("b", rep("a", 4))) %>% 
+  ggplot(., aes(x = x, y = y)) + 
+    geom_point(aes(size = size, shape = ident, color = ident), 
+      show.legend = F, stroke = 15) + 
+    scale_shape_manual(values = c(21, 16)) + 
+    scale_size_identity() + 
+    scale_color_manual(values = c(the_colors[2, 2] %>% pull, 
+                                  the_colors[2, 3] %>% pull)) + 
     coord_cartesian(xlim = c(-0.4, 0.4), ylim = c(-0.4, 0.4), expand = F) + 
     theme_clear()
 
@@ -93,26 +221,19 @@ ggsave("high_complexity_static_circle.png", high_complexity_static_circle,
   path = here("stim"), units = "in", height = 5, width = 5)
 
 # Expanding movement
-high_complexity_movement_circle <- bind_rows(
-  make_circle(diameter = 0.01, npoints = 200) %>% mutate(s = 1), 
-  make_circle(diameter = 0.25, npoints = 200) %>% mutate(s = 1),
-  make_circle(diameter = 0.50, npoints = 200) %>% mutate(s = 1), 
-  make_circle(diameter = 0.25, npoints = 200) %>% mutate(s = 2), 
-  make_circle(diameter = 0.50, npoints = 200) %>% mutate(s = 2), 
-  make_circle(diameter = 0.75, npoints = 200) %>% mutate(s = 2)) %>% 
-  mutate(position = "outer") %>% 
-  add_row(x = 0, y = 0, s = 1, position = "inner") %>% 
-  add_row(x = 0, y = 0, s = 2, position = "inner") %>% 
-  ggplot(., aes(x = x, y = y, size = position)) + 
-    geom_point(
-      aes(alpha = if_else(abs(x) >= 0.252 & position == "outer" | 
-                          abs(y) >= 0.255 & position == "outer" , 0.0, 1)), 
-      show.legend = F) + 
-    scale_size_manual(values = c(10, 3)) + 
+
+high_complexity_movement_circle <-
+  read_csv(here("data", "high_complexity_movement_circle.csv")) %>% 
+  ggplot(., aes(x = x, y = y)) + 
+    geom_point(aes(size = size, shape = color, color = color), 
+      show.legend = F, stroke = 15) + 
+    scale_shape_manual(values = c(21, 16)) + 
+    scale_size_identity() + 
+    scale_color_manual(values = c(the_colors[2, 2] %>% pull, 
+                                  the_colors[2, 3] %>% pull)) + 
     coord_cartesian(xlim = c(-0.4, 0.4), ylim = c(-0.4, 0.4), expand = F) + 
     theme_clear() + 
-    transition_states(s, transition_length = 0.5, state_length = 0, wrap = F) + 
-    exit_fade(alpha = 0.2)
+    transition_states(s, transition_length = 0.5, state_length = 0, wrap = F)
 
 # Animate
 high_complexity_movement_circle_p <- animate(high_complexity_movement_circle, 
@@ -124,3 +245,9 @@ anim_save(here("stim", "high_complexity_movement_circle.gif"),
   high_complexity_movement_circle_p)
 
 # -----------------------------------------------------------------------------
+
+
+
+
+
+
