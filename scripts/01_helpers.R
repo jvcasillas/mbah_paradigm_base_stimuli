3bca17465851b97158b2d1f9d38cbbecd5a60490
jvcasillas <joseph.casillas@rutgers.edu>
2020-12-02 18:35:12 -0500

    add color picker function

--- scripts/01_helpers.R
+++ scripts/01_helpers.R
@@ -14,6 +14,26 @@ source(here::here("scripts", "00_libs.R"))
 
 
 
+
+# helpers ---------------------------------------------------------------------
+
+# Download colors
+if (F) {
+  file_url <- "https://raw.githubusercontent.com/jvcasillas/MBAH_ColorSwatches/master/MBAH_polarLUV-hex_L_C.csv"
+  download.file(file_url, destfile = here("data", "colors.csv"), method = "curl")
+}
+
+# Read in csv of colors
+the_colors <- read_delim(here("data", "colors.csv"), delim = ";")
+
+# Color picker function
+c_picker <- function(pair = 1) {
+  out <- c(the_colors[the_colors$X1 == pair, 2] %>% pull, 
+  the_colors[the_colors$X1 == pair, 3] %>% pull)
+  return(out)
+}
+
+
 # Circle maker 
 make_circle <- function(center = c(0, 0), diameter = 1, npoints = 100){
 
@@ -26,7 +46,7 @@ make_circle <- function(center = c(0, 0), diameter = 1, npoints = 100){
 }
 
 # ggplot2 theme
-theme_clear <- function(bg_color = NULL) {
+theme_clear <- function(bg_color = the_colors[1, 2] %>% pull) {
   list(
     theme_void(), 
     theme(panel.background = element_rect(fill = bg_color, colour = bg_color))
